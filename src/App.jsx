2489082f8fdefed4cbf6e7d6f2907344e37ce408
marc7hedev <driberstagg@gmail.com>
2024-07-27 16:11:30 -0600

    Definidas variables en la propiedad theme de tailwind.config.js

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,16 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div className="container-md mx-auto grid grid-cols-1 md:grid-cols-3 gap-4 p-3 md:p-7">
-        <div className="border border-red-500 text-amber-800">column 1</div>
-        <div className="border border-black col-span-2">column 2</div>
+
+        <div className="border border-border text-[#243c5a]">column 1</div>
+        <div className="border border-background col-span-2">column 2</div>
+      
       </div>
     </>
-  )
+  );
 }
-
 export default App
