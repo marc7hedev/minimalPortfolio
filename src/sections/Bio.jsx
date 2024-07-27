c6b272e1c336ec7e8396771b187027d840e1e17c
marc7hedev <driberstagg@gmail.com>
2024-07-27 17:52:59 -0600

    Implementado enlaces en los iconos de contacto

--- src/sections/Bio.jsx
+++ src/sections/Bio.jsx
@@ -1,4 +1,4 @@
-import { Linkedin } from "lucide-react";
+import { Linkedin, AtSign, GitBranch } from "lucide-react";
 
 const Bio = () => {
     
@@ -7,6 +7,19 @@ const Bio = () => {
         
             <h1 className="text-lg md:text-2xl font-bold mt-3">Marco Rangel</h1>
             <p className="text-secondary-foreground py-2">Desarrollador Web especializado en Backend</p>
+            <div className="flex flex-row gap-2 mt-2">
+                <a href="https://mx.linkedin.com/" target="_blank" rel="noopener noreferrer" >
+
+                    <Linkedin className="text-blue-400 hover:text-blue-600 cursor-pointer"/>
+
+                </a>
+                <a href="https://mx.linkedin.com/" target="_blank" rel="noopener noreferrer" >
+                    <AtSign className="text-pink-400 hover:text-pink-600 cursor-pointer"/>
+                </a>
+                <a href="https://github.com/marc7hedev" target="_blank" rel="noopener noreferrer" >
+                    <GitBranch className="text-red-400 hover:text-red-600 cursor-pointer"/>
+                </a>
+            </div>
         
         </section>
     )
