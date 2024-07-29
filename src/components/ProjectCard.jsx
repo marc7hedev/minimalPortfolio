c4d34eb28455333fcb062181f9834f75736f267c
marc7hedev <driberstagg@gmail.com>
2024-07-29 14:51:37 -0600

    Arreglados paddings de sitio en general

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -3,7 +3,7 @@ import { ChevronRight } from "lucide-react";
 
 const ProjectCard = ({ name, description, url, imgUrl }) => {
     return (
-        <a className="flex border-b py-8 px-rounded-md gap-4 group/item hover:bg-slate-100 mt-1" href={url} target="_blank">
+        <a className="flex border-b py-5 px-rounded-md gap-4 group/item hover:bg-slate-100 mt-1" href={url} target="_blank">
             <img 
             src={imgUrl} 
             alt={name} 
