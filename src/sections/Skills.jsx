23aaed36ecb1037dff632d5ba5cb6c3624191d5f
marc7hedev <driberstagg@gmail.com>
2024-07-29 13:41:15 -0600

    Corregido estilos en Skills.jsx

--- src/sections/Skills.jsx
+++ src/sections/Skills.jsx
@@ -22,7 +22,7 @@ const skills = [
 const Skills = () => {
     return (
         <section className="bg-white text-left rounded-xl p-2 md:p-5 shadow-md mt-6">
-            <h2 className="text-lg md:text-2xl text-secondary-foreground mb-3 font-semibold">Skills</h2>
+            <h2 className="text-lg md:text-2xl text-secondary-foreground mb-3 font-semibold text-center md:text-left">Skills</h2>
             <ul className="flex flex-wrap leading-10">
                 {
                     skills.map((skill, index) => (
